bd5aeb64c011794a48441ceddd19d9b30d375137
矢部巧磨 <@MSI>
2023-04-17 13:06:22 +0900

    1-2の確認課題を取り組んだ。

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -1,19 +1,66 @@
 #include "GameScene.h"
 #include "TextureManager.h"
 #include <cassert>
-
+#include "imGuiManager.h"
+#include "PrimitiveDrawer.h"
+#include "AxisIndicator.h"
 GameScene::GameScene() {}
 
-GameScene::~GameScene() {}
+GameScene::~GameScene() {
+	delete sprite_;
+	delete model_;
+	delete debugCamera_;
+}
 
 void GameScene::Initialize() {
 
 	dxCommon_ = DirectXCommon::GetInstance();
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
+	textureHandle_ = TextureManager::Load("yoshi.jpg");
+	sprite_ = Sprite::Create(textureHandle_, {10, 0});
+	model_ = Model::Create();
+	worldTransform_.Initialize();
+	viewProjection_.Initialize();
+	//サウンド
+	soundDataHandle_ = audio_->LoadWave("mokugyo.wav");
+	audio_->PlayWave(soundDataHandle_);
+	voiceHandle_ = audio_->PlayWave(soundDataHandle_, true);
+	//ライン描画が参照するビュープロジェクションを指定する(アドレス渡し)
+	PrimitiveDrawer::GetInstance()->SetViewProjection(&viewProjection_);
+	//デバッグカメラの生成
+	debugCamera_ = new DebugCamera(200, 200);
 }
 
-void GameScene::Update() {}
+void GameScene::Update() { 
+
+	Vector2 position = sprite_->GetPosition();
+	position.x += 2.0f;
+	position.y += 1.0f;
+
+	sprite_->SetPosition(position);
+	if (input_->TriggerKey(DIK_SPACE)) {
+		audio_->StopWave(voiceHandle_);
+	}
+	//デバッグテキストの表示
+	ImGui::Begin("Debug1");
+	
+	ImGui::Text("Kamata Tarou %d %d %d", 2050, 12, 31);
+	//float3入力ボックス
+	ImGui::InputFloat3("InputFloat3", inputFloat3);
+	//float3スライダー
+	ImGui::SliderFloat3("SliderFloat3", inputFloat3, 0.0f, 1.0f);
+
+	ImGui::End();
+
+	//ImGui::ShowDemoWindow();
+	//デバッグカメラの更新
+	debugCamera_->Update();
+	//軸方向表示を有効にする
+	AxisIndicator::GetInstance()->SetVisible(true);
+	//軸方向表示が参照するビュープロジェクションを指定する(アドレス渡し)
+	AxisIndicator::GetInstance()->SetTargetViewProjection(&debugCamera_->GetViewProjection());
+}
 
 void GameScene::Draw() {
 
@@ -27,7 +74,7 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに背景スプライトの描画処理を追加できる
 	/// </summary>
-
+	PrimitiveDrawer::GetInstance()->DrawLine3d({30, 0, 0}, {10, 0, 0}, {1.0f,0.0f,0.0f,1.0f});
 	// スプライト描画後処理
 	Sprite::PostDraw();
 	// 深度バッファクリア
@@ -41,7 +88,8 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
-
+	
+	model_->Draw(worldTransform_, debugCamera_->GetViewProjection(), textureHandle_);
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
 #pragma endregion
@@ -53,9 +101,11 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに前景スプライトの描画処理を追加できる
 	/// </summary>
-
+	sprite_->Draw();
 	// スプライト描画後処理
 	Sprite::PostDraw();
+
+
 
 #pragma endregion
 }
--- scene/GameScene.h
+++ scene/GameScene.h
@@ -7,13 +7,15 @@
 #include "SafeDelete.h"
 #include "Sprite.h"
 #include "ViewProjection.h"
+#include "DebugCamera.h"
 #include "WorldTransform.h"
 
 /// <summary>
 /// ゲームシーン
 /// </summary>
 class GameScene {
-
+	
+	
 public: // メンバ関数
 	/// <summary>
 	/// コンストクラタ
@@ -44,7 +46,18 @@ private: // メンバ変数
 	DirectXCommon* dxCommon_ = nullptr;
 	Input* input_ = nullptr;
 	Audio* audio_ = nullptr;
-
+	uint32_t textureHandle_ = 0;
+	Sprite* sprite_ = nullptr;
+	Model* model_ = nullptr;
+	WorldTransform worldTransform_;
+	ViewProjection viewProjection_;
+	//サウンド
+	uint32_t soundDataHandle_ = 0;
+	uint32_t voiceHandle_ = 0;
+	//ImGuiで値を入力する変数
+	float inputFloat3[3] = {0, 0, 0};
+	//デバッグカメラ
+	DebugCamera* debugCamera_ = nullptr;
 	/// <summary>
 	/// ゲームシーン用
 	/// </summary>
