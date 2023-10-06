2c568005ac722fb9881dfee4359d68ee47cec6ea
yabe-takuma <115063061+yabe-takuma@users.noreply.github.com>
2023-10-06 15:01:49 +0900

    数学関数を追加している途中

--- MathFunction.h
+++ MathFunction.h
@@ -27,6 +27,23 @@ class MathFunction {
 
 	Vector3 Transform(const Vector3& vector, const Matrix4x4& matrix);
 
+	Vector3 Add(const Vector3& v1, const Vector3& v2);
 
+	Vector3 Subtract(const Vector3& v1, const Vector3& v2);
+
+	Vector3 Normalize(const Vector3& v);
+
+	Vector3 Multiply(float scalar, const Vector3& v);
+
+	bool CollisionDot(
+	    const Vector3& v1, const Vector3& v2, const Vector3& radius, const Vector3& radius2);
+
+	float Dot(const Vector3& v1, const Vector3& v2);
+
+	float Add2(const Vector3& v1, const Vector3& v2);
+
+	Vector3 Multiply2(const Vector3& v1, const Vector3& v2);
+
+	Vector3 Subtract2(float position, const Vector3& v);
 
 };
