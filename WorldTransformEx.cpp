f5d643ee37fa3a1ccd3ceb89c5d00f92dbfede73
yabe-takuma <115063061+yabe-takuma@users.noreply.github.com>
2023-10-06 15:06:49 +0900

    新しいcppを作成した。

---
+++ WorldTransformEx.cpp
@@ -0,0 +1,18 @@
+#include "Matrix4x4.h"
+#include "Vector3.h"
+#include "WorldTransform.h"
+
+void WorldTransform::UpdateMatrix() {
+	matWorld_.m;
+
+	// スケール、回転、平行移動を合成して行列を計算する
+	matWorld_ = MakeAffineMatrix(scale_, rotation_, translation_);
+
+	// 親があれば親のワールド行列に掛ける
+	if (parent_) {
+
+		matWorld_ = Multiply(matWorld_, parent_->matWorld_);
+	}
+
+	TransferMatrix();
+}
