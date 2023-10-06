db5e0bc014743fb0e00f672f58fec65999d3f6e2
yabe-takuma <115063061+yabe-takuma@users.noreply.github.com>
2023-10-06 16:22:48 +0900

    AL4の1_02をやっているところ

--- Skydome.cpp
+++ Skydome.cpp
@@ -6,6 +6,20 @@ void Skydome::Initialize(Model* model) {
 
 	    model_ = model;
 
-
+		worldTransform_.translation_ = {0.0f, 0.0f, 0.0f};
+	    worldTransform_.scale_ = {100.0f, 100.0f, 100.0f};
+	    worldTransform_.rotation_ = {0.0f, 0.0f, 0.0f};
+	    worldTransform_.Initialize();
 
 }
+
+void Skydome::Update() {
+	    worldTransform_.TransferMatrix();
+	worldTransform_.UpdateMatrix();
+	    
+}
+
+void Skydome::Draw(ViewProjection& viewProjection) {
+	model_->Draw(worldTransform_, viewProjection);
+}
+
