28aa34ce52b8d13949a2f967f7141ffd77bf7543
yabe-takuma <115063061+yabe-takuma@users.noreply.github.com>
2023-10-13 15:27:02 +0900

    数学関数を追加

--- math/Vector3.h
+++ math/Vector3.h
@@ -8,3 +8,22 @@ struct Vector3 final {
 	float y;
 	float z;
 };
+
+Vector3 Add(const Vector3& v1, const Vector3& v2);
+
+Vector3 Subtract(const Vector3& v1, const Vector3& v2);
+
+Vector3 Normalize(const Vector3& v);
+
+Vector3 Multiply(float scalar, const Vector3& v);
+
+bool CollisionDot(
+    const Vector3& v1, const Vector3& v2, const Vector3& radius, const Vector3& radius2);
+
+float Dot(const Vector3& v1, const Vector3& v2);
+
+float Add2(const Vector3& v1, const Vector3& v2);
+
+Vector3 Multiply2(const Vector3& v1, const Vector3& v2);
+
+Vector3 Subtract2(float position, const Vector3& v);
