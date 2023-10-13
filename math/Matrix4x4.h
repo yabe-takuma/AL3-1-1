8d392162eb1d6f1454703796850dfebb53748292
yabe-takuma <115063061+yabe-takuma@users.noreply.github.com>
2023-10-13 15:28:04 +0900

    数学関数を追加

--- math/Matrix4x4.h
+++ math/Matrix4x4.h
@@ -1,7 +1,31 @@
 #pragma once
+#include"Vector3.h"
 /// <summary>
 /// 4x4行列
 /// </summary>
 struct Matrix4x4 final {
 	float m[4][4];
 };
+
+Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2);
+
+Matrix4x4 MakeRotateXMatrix(float radian);
+
+Matrix4x4 MakeRotateYMatrix(float radian);
+
+Matrix4x4 MakeRotateZMatrix(float radian);
+
+Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3 rotate, const Vector3& translate);
+
+Vector3 TransformNormal(const Vector3& v, const Matrix4x4& m);
+
+Matrix4x4 Inverse(Matrix4x4& m);
+
+Matrix4x4 Multiply2(const Vector3& v, const Matrix4x4& m);
+
+Matrix4x4 Subtract(const Matrix4x4& m1, const Matrix4x4& m2);
+
+Matrix4x4 MakeViewportMatrix(
+    float left, float top, float width, float height, float minDepth, float maxDepth);
+
+Vector3 Transform(const Vector3& vector, const Matrix4x4& matrix);
