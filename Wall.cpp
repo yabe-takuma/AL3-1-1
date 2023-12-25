f1ba294a52a297129c6bcda4275ee6510ce80071
yabe-takuma <115063061+yabe-takuma@users.noreply.github.com>
2023-12-25 16:03:30 +0900

    AL4の評価課題をやっているところ

--- Wall.cpp
+++ Wall.cpp
@@ -1,1 +1,10 @@
 #include "Wall.h"
+
+void Wall::Initialize() {
+	worldTransform_.Initialize();
+	model_ = Model::Create();
+}
+
+void Wall::Update() { worldTransform_.UpdateMatrix(); }
+
+void Wall::Draw(ViewProjection& viewProjection) { model_->Draw(worldTransform_, viewProjection); }
