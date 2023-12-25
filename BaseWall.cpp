d7f0a011d7fa9c45bf7a531c4a94c02d1c2ce627
yabe-takuma <115063061+yabe-takuma@users.noreply.github.com>
2023-12-25 16:07:57 +0900

    AL4の評価課題をやっているところ

--- BaseWall.cpp
+++ BaseWall.cpp
@@ -1,1 +1,14 @@
 #include "BaseWall.h"
+
+void BaseWall::Initialize(const std::vector<Model*>& models) {
+	models_ = models;
+	worldTransform_.Initialize();
+}
+
+void BaseWall::Update() { worldTransform_.UpdateMatrix(); }
+
+void BaseWall::Draw(ViewProjection& viewProjection) {
+	for (Model* model : models_) {
+		model->Draw(worldTransform_, viewProjection);
+	}
+}
