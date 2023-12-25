f1ac4c71fe9d680f033fb5d095657ff5cddd4805
yabe-takuma <115063061+yabe-takuma@users.noreply.github.com>
2023-12-25 16:16:06 +0900

    AL4の評価課題をやっているところ

--- Wall.cpp
+++ Wall.cpp
@@ -5,9 +5,13 @@ void Wall::Initialize(const std::vector<Model*>& models) {
 	BaseWall::Initialize(models);
 	
 	worldTransform_.Initialize();
-	model_ = Model::Create();
+	
 }
 
-void Wall::Update() { worldTransform_.UpdateMatrix(); }
+void Wall::Update() {
+	BaseWall::Update();
+	worldTransform_.UpdateMatrix(); 
 
-void Wall::Draw(ViewProjection& viewProjection) { model_->Draw(worldTransform_, viewProjection); }
+}
+
+void Wall::Draw(ViewProjection& viewProjection) { models->Draw(worldTransform_, viewProjection); }
