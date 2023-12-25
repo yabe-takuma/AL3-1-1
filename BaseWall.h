3e3544fc044be071c8ba30b5b3b1a4bb7aaf2950
yabe-takuma <115063061+yabe-takuma@users.noreply.github.com>
2023-12-25 16:06:40 +0900

    AL4の評価課題をやっているところ

--- BaseWall.h
+++ BaseWall.h
@@ -1,2 +1,33 @@
 #pragma once
-class BaseWall {};
+#include "Model.h"
+#include <vector>
+class BaseWall {
+public:
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	/// <param name="models_">モデルデータ配列</param>
+	virtual void Initialize(const std::vector<Model*>& models);
+
+	/// <summary>
+	/// 更新
+	/// </summary>
+	virtual void Update();
+
+	/// <sumamry>
+	/// 描画
+	/// </sumamry>
+	/// <param name="viewProjection">ビュープロジェクション (参照渡し) </param>
+	virtual void Draw(ViewProjection& viewProjection);
+
+	/// <summary>
+	/// ワールド変換データを取得
+	/// </summary>
+	const WorldTransform& GetWorldTransform() { return worldTransform_; }
+
+protected:
+	// モデルデータ
+	std::vector<Model*> models_;
+	// ワールド変換データ
+	WorldTransform worldTransform_;
+};
