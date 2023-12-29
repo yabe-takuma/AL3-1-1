f0bb64f31ed4f3edeb30c53fb3c3b2da3e25a814
yabe-takuma <115063061+yabe-takuma@users.noreply.github.com>
2023-12-29 17:23:09 +0900

    AL4の評価課題をやっているところ

--- BaseCharacter.cpp
+++ BaseCharacter.cpp
@@ -1,1 +1,13 @@
 #include "BaseCharacter.h"
+void BaseCharacter::Initialize(const std::vector<Model*>& models) {
+	models_ = models;
+	worldTransform_.Initialize();
+}
+
+void BaseCharacter::Update() { worldTransform_.UpdateMatrix(); }
+
+void BaseCharacter::Draw(ViewProjection& viewProjection) {
+	for (Model* model : models_) {
+		model->Draw(worldTransform_, viewProjection);
+	}
+}
