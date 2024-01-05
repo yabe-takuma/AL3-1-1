a960bb4c256e92b7165df34fe41aa8517c725046
yabe-takuma <115063061+yabe-takuma@users.noreply.github.com>
2024-01-06 02:22:14 +0900

    AL4の評価課題をやっているところ

--- TitleScene.h
+++ TitleScene.h
@@ -1,2 +1,55 @@
 #pragma once
-class TitleScene {};
+#include "Audio.h"
+#include "DirectXCommon.h"
+#include "Input.h"
+#include "Model.h"
+#include "SafeDelete.h"
+#include "Scene.h"
+#include "Sprite.h"
+#include "ViewProjection.h"
+#include "WorldTransform.h"
+
+/// <summary>
+/// タイトルシーン
+/// </summary>
+class TitleScene {
+
+public: // メンバ関数
+	/// <summary>
+	/// コンストラクタ
+	/// </summary>
+	TitleScene();
+
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
+	~TitleScene();
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize();
+
+	/// <summary>
+	/// 毎フレーム処理
+	/// </summary>
+	void Update();
+
+	///< summary>
+	/// 描画
+	/// </summary>
+	void Draw();
+
+	void DrawUI();
+
+	bool IsSceneEnd() { return isSceneEnd_; }
+	Scene::SceneType NextScene() { return Scene::SceneType::kGameExplanation; }
+
+private: // メンバ変数
+	DirectXCommon* dxCommon_ = nullptr;
+	Input* input_ = nullptr;
+	Audio* audio_ = nullptr;
+	bool isSceneEnd_ = false;
+
+	//Sprite* titlesprite_ = nullptr;
+};
