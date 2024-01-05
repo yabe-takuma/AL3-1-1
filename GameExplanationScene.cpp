6246149cb6d4d9b5952c6549189eed83a089d4be
yabe-takuma <115063061+yabe-takuma@users.noreply.github.com>
2024-01-06 02:30:44 +0900

    AL4の評価課題をやっているところ

--- GameExplanationScene.cpp
+++ GameExplanationScene.cpp
@@ -1,1 +1,50 @@
 #include "GameExplanationScene.h"
+#include "Audio.h"
+#include "DirectXCommon.h"
+#include "Input.h"
+#include "Model.h"
+#include "SafeDelete.h"
+#include "Scene.h"
+#include "Sprite.h"
+#include "ViewProjection.h"
+#include "WorldTransform.h"
+class GameExplanationScene {
+public: // メンバ関数
+	/// <summary>
+	/// コンストラクタ
+	/// </summary>
+	GameExplanationScene();
+
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
+	~GameExplanationScene();
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize();
+
+	/// <summary>
+	/// 毎フレーム処理
+	/// </summary>
+	void Update();
+
+	///< summary>
+	/// 描画
+	/// </summary>
+	void Draw();
+
+	void DrawUI();
+
+	bool IsSceneEnd() { return isSceneEnd_; }
+	Scene::SceneType NextScene() { return Scene::SceneType::kGamePlay; }
+
+private: // メンバ変数
+	DirectXCommon* dxCommon_ = nullptr;
+	Input* input_ = nullptr;
+	Audio* audio_ = nullptr;
+	bool isSceneEnd_ = false;
+
+	//Sprite* titlesprite_ = nullptr;
+};
