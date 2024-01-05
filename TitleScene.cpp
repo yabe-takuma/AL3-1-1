f873334a832bfe77e46cab20dfd97802aa586426
yabe-takuma <115063061+yabe-takuma@users.noreply.github.com>
2024-01-06 02:28:12 +0900

    AL4の評価課題をやっているところ

--- TitleScene.cpp
+++ TitleScene.cpp
@@ -1,1 +1,72 @@
 #include "TitleScene.h"
+
+TitleScene::TitleScene() {}
+
+TitleScene::~TitleScene(){
+
+};
+
+void TitleScene::Initialize() {
+	dxCommon_ = DirectXCommon::GetInstance();
+	input_ = Input::GetInstance();
+	audio_ = Audio::GetInstance();
+
+	//uint32_t textureTitle = TextureManager::Load("Title2.png");
+
+	/*titlesprite_ =
+	    Sprite::Create(textureTitle, {640.0f, 360.0f}, {1.0f, 1.0f, 1.0f, 1.0f}, {0.5f, 0.5f});*/
+}
+
+void TitleScene::Update() {
+	if (input_->PushKey(DIK_A)) {
+		isSceneEnd_ = true;
+	}
+}
+
+void TitleScene::Draw() {
+	// コマンドリストの取得
+	ID3D12GraphicsCommandList* commandList = dxCommon_->GetCommandList();
+
+#pragma region 背景スプライト描画
+	// 背景スプライト描画前処理
+	Sprite::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに背景スプライトの描画処理を追加できる
+	/// </summary>
+
+	// スプライト描画後処理
+	Sprite::PostDraw();
+	// 深度バッファクリア
+	dxCommon_->ClearDepthBuffer();
+#pragma endregion
+
+#pragma region 3Dオブジェクト描画
+	// 3Dオブジェクト描画前処理
+	Model::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに3Dオブジェクトの描画処理を追加できる
+	/// </summary>
+
+	// 3Dオブジェクト描画後処理
+	Model::PostDraw();
+#pragma endregion
+
+#pragma region 前景スプライト描画
+	// 前景スプライト描画前処理
+	Sprite::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに前景スプライトの描画処理を追加できる
+	/// </summary>
+
+	//DrawUI();
+
+	// スプライト描画後処理
+	Sprite::PostDraw();
+
+#pragma endregion
+}
+
+void TitleScene::DrawUI() { //titlesprite_->Draw(); }
