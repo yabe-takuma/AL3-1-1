c77ac8fd5da75887d262fa1cd05cd1e8a05142c6
yabe-takuma <115063061+yabe-takuma@users.noreply.github.com>
2024-01-06 02:55:45 +0900

    AL4の評価課題をやっているところ

--- GameClear.h
+++ GameClear.h
@@ -1,2 +1,56 @@
 #pragma once
-class GameClear {};
+#include "Audio.h"
+#include "DirectXCommon.h"
+#include "Input.h"
+#include "Model.h"
+
+#include "SafeDelete.h"
+#include "Scene.h"
+#include "Sprite.h"
+#include "ViewProjection.h"
+#include "WorldTransform.h"
+
+class GameClear {
+public: // メンバ関数
+	/// <summary>
+	/// コンストラクタ
+	/// </summary>
+	GameClear();
+
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
+	~GameClear();
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize();
+
+	/// <summary>
+	/// 毎フレーム処理
+	/// </summary>
+	void Update();
+
+	///< summary>
+	/// 描画
+	/// </summary>
+	void Draw();
+
+	void DrawUI();
+
+	bool IsSceneEnd() { return isSceneEnd_; }
+	Scene::SceneType NextScene() { return Scene::SceneType::kGamePlay; }
+
+private: // メンバ変数
+	DirectXCommon* dxCommon_ = nullptr;
+	Input* input_ = nullptr;
+	Audio* audio_ = nullptr;
+	bool isSceneEnd_ = false;
+
+	//Sprite* titlesprite_ = nullptr;
+
+	// Sprite* mojisprite_ = nullptr;
+
+	
+};
