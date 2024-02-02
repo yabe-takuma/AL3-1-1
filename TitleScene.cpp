c24f00ae4282393b36a6793f0af6ef65d6e594d3
yabe-takuma <115063061+yabe-takuma@users.noreply.github.com>
2024-02-02 16:53:01 +0900

    AL4の評価課題をやっているところ

--- TitleScene.cpp
+++ TitleScene.cpp
@@ -102,4 +102,17 @@ void TitleScene::DrawUI() { titlesprite_->Draw();
 
 void TitleScene::Reset() { isSceneEnd_ = false; }
 
+float TitleScene::easeOutBounce(float x) {
+	const float n1 = 7.5625f;
+	const float d1 = 2.75f;
 
+	if (x < 1 / d1) {
+		return n1 * x * x;
+	} else if (x < 2 / d1) {
+		return n1 * (x -= 1.5f / d1) * x + 0.75f;
+	} else if (x < 2.5f / d1) {
+		return n1 * (x -= 2.25f / d1) * x + 0.9375f;
+	} else {
+		return n1 * (x -= 2.625f / d1) * x + 0.984375f;
+	}
+}
